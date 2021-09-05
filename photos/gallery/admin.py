9fa8d55baa9bfe6a83f224c83269fd6a9968ebc8
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-05 21:27:27 +0300

    class ImageAdmin

--- photos/gallery/admin.py
+++ photos/gallery/admin.py
@@ -9,3 +9,15 @@ from .models import Location, Category, Image
 
 admin.site.register(Location)
 admin.site.register(Category)
+
+class ImageAdmin(admin.ModelAdmin):
+    date_hierarchy = 'pub_date'
+    list_display       = ['image_name', 'location', 'category', 'active', 'pub_date']
+    list_display_links = ['image_name']
+    list_filter   = ['location', 'category', 'active']
+    search_fields = ['location__location', 'category__category']
+    readonly_fields = ['pub_date']
+    list_per_page = 25
+    class Meta:
+        model = Image
+admin.site.register(Image, ImageAdmin)
