aeb18b6537a051d790e4e98ae144d9f331a9e875
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-05 21:31:09 +0300

    class location

--- photos/gallery/models.py
+++ photos/gallery/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    location = models.CharField(max_length= 255, blank =False, null=False)
+    active   = models.BooleanField(default=True)
+    
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+    
+    @classmethod
+    def get_location(cls):
+        locations = cls.objects.all()
+        return locations
+    
+    def __str__(self):
+        return self.location
+    
+    class Meta:
+        verbose_name = 'Location'
+        verbose_name_plural = 'Locations'
