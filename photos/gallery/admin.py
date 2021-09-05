dccf2b9fd69cc9949d5d2f30d64c742e1c506196
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-05 21:26:30 +0300

    config admin

---
+++ photos/gallery/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Location, Category, Image
+
+
+# Register your models here.
+
+
+
+
+admin.site.register(Location)
+admin.site.register(Category)
