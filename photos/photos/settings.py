38c670479cab8d6ff427a3d3898c37074cc4338e
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-06 17:08:10 +0300

    test

--- photos/photos/settings.py
+++ photos/photos/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*q_c*y#imfq_5eds=x719fe-wqd(_zay&w#0k)#k=b7gwgtkqu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
